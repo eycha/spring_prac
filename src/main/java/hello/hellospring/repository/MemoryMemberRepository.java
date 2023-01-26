8119e01eeac807cefc6e27d9f1e939100ab84a8c
eycha <silvlexonst@gmail.com>
2023-01-26 18:55:41 +0900

    feat: DB연결하기

--- src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemoryMemberRepository.java
@@ -7,6 +7,15 @@ import java.util.*;
 
 @Repository
 public class MemoryMemberRepository implements MemberRepository{
+//    private static Map<Long, Member> store = new HashMap<>();
+//    private  static long sequence = 0L;
+//
+//    @Override
+//    public Member save(Member member) {
+//        member.setId(++sequence);
+//        store.put(member.getId(),member);
+//        return member;
+//    }
 
     private static Map<Long, Member> store = new HashMap<>();
     private static Long sequence = 0L;
