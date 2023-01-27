953f093a25379bcfc39989152dbd66830f0046fe
eycha <silvlexonst@gmail.com>
2023-01-27 12:44:46 +0900

    typo fix

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -6,7 +6,6 @@ import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
