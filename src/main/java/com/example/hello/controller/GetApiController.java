b40fb5381f7881ec1f4d1959517e03b8e9d26703
eycha <silvlexonst@gmail.com>
2023-03-21 20:35:36 +0900

    query-param

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -2,6 +2,8 @@ package com.example.hello.controller;
 
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/get")
 public class GetApiController {
@@ -21,5 +23,18 @@ public class GetApiController {
         System.out.println("PathVariable:" + pathName);
         return pathName;
 
+    }
+    @GetMapping(path="query-param")
+    public String queryParam(Map<String, String> queryParam) {
+        StringBuilder sb = new StringBuilder();
+
+        queryParam.entrySet().forEach(entry-> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey()+"="+entry.getValue()+"\n");
+        });
+        return sb.toString();
     }
 }
