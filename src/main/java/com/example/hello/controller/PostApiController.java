9c37b853a86cc86e5fcc0c43ce9edbcde0accebe
eycha <silvlexonst@gmail.com>
2023-03-23 22:59:18 +0900

    poest request DTO

--- src/main/java/com/example/hello/controller/PostApiController.java
+++ src/main/java/com/example/hello/controller/PostApiController.java
@@ -1,5 +1,8 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.PostRequestDto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,10 +11,8 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api")
 public class PostApiController {
-    public void post(Map<String, Object> requestData) {
-        requestData.forEach((key, value) -> {
-            System.out.println("key : " + key);
-            System.out.println("value : " + value);
-        });
+    @PostMapping("/post")
+    public void post(@RequestBody PostRequestDto requestData) {
+        System.out.println(requestData);
     }
 }
